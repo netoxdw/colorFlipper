ebaea87c7ce3a9b397f89a630a529f4ad79f9fa8
netoxdw <neto.guevara.tapia@gmail.com>
2022-10-31 00:45:30 -0600

    Iniciando proyecto

---
+++ app.js
@@ -0,0 +1,18 @@
+// array 
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+// get element by id at document html
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
